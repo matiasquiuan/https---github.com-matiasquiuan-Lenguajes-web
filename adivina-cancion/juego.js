9971631bc25f56d2bbea679fe54f65f391bbe11f
matiasquiuan <72324376+matiasquiuan@users.noreply.github.com>
2026-06-09 23:58:05 -0300

    sistema de verificacion de respuesta y puntaje

--- adivina-cancion/juego.js
+++ adivina-cancion/juego.js
@@ -2,6 +2,7 @@ import songs from './canciones.js'; //importamos todas las canciones desde el ar
 
 let ytPlayer;
 let playerListo = false;
+const DURACION_CLIP = 10; // duración del fragmento en segundos
 
 window.onYouTubeIframeAPIReady = function() {
     ytPlayer = new YT.Player("yt-player", {
@@ -32,8 +33,8 @@ function juegoInicio(){
     let btn = document.getElementById("btn");
     btn.addEventListener("click", function() {
         console.log("Reproducir fragmento");
-        const randomSong = songs[Math.floor(Math.random() * songs.length)];//obtengo una canción aleatoria
-        console.log("Canción seleccionada:", randomSong.videoId);
+        var randomSong = songs[Math.floor(Math.random() * songs.length)];//obtengo una canción aleatoria
+        console.log("Canción seleccionada:", randomSong);
         if (!ytPlayer || !playerListo) { // verifico que el player este listo
             console.warn("player no listo");
             return;
@@ -45,7 +46,7 @@ function juegoInicio(){
         ytPlayer.playVideo();
         btn.disabled = true;
         btn.textContent = "🎵 Escuchando...";
-        // Parar después de DURACION_CLIP segundos
+        // parar despues de DURACION_CLIP segundos
         stopTimer = setTimeout(() => {
             ytPlayer.stopVideo();
             btn.disabled = false;
@@ -55,6 +56,26 @@ function juegoInicio(){
     
 }
 
+function verificarRespuesta(respuesta) {
+    let respuesta = document.getElementById("respuesta");
+    let puntos = document.getElementById("puntos");
+    if(respuesta.value.toLowerCase() === randomSong.name.toLowerCase()){
+        // respuesta correcta
+        console.log("Respuesta correcta");
+        puntos.textContent = parseInt(puntos.textContent) + 1; // sumo un punto
+        btn.textContent = "▶ Volver a jugar";
+        btn.addEventListener("click", function() {
+            juegoInicio();
+        });
+    }else{
+        // respuesta incorrecta
+        console.log("Respuesta incorrecta"); 
+        btn.textContent = "▶ Volver a jugar";
+        btn.addEventListener("click", function() {
+            juegoInicio();
+        });
+    }
+}
 
 // Arrancar al cargar
 juegoInicio();
